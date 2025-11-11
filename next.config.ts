25f1b28a05d972773f2ee530e9562e94500545f8
vilebbit <50281195+vilebbit@users.noreply.github.com>
2025-11-11 13:00:24 +0000

    chore: change nextjs config chore: add new packages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig, } from "next"
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://pics.dmm.co.jp/**"),
+      new URL("https://www.suruga-ya.jp/database/pics/game/**"),
+      new URL("https://img.dlsite.jp/**"),
+    ],
+  },
 }
 
 export default nextConfig
