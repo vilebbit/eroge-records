7533f6cf6f817421569c8bb337ce4f41e37d2eda
vilebbit <50281195+vilebbit@users.noreply.github.com>
2025-11-12 15:27:17 +0000

    chore: add CSP

--- next.config.ts
+++ next.config.ts
@@ -4,10 +4,27 @@ const nextConfig: NextConfig = {
   cacheComponents: true,
   images: {
     remotePatterns: [
+      new URL("https://img.vibbit.me/**"),
       new URL("https://pics.dmm.co.jp/**"),
       new URL("https://www.suruga-ya.jp/database/pics/game/**"),
       new URL("https://img.dlsite.jp/**"),
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: `
+              connect-src 'self' *.vibbit.me;
+              img-src 'self' blob: data: img.vibbit.me pics.dmm.co.jp www.suruga-ya.jp img.dlsite.jp;
+            `.replace(/\s{2,}/g, " ").trim(),
+          },
+        ],
+      },
+    ]
   },
 }
 
