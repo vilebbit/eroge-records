2db5bee71435fbf9781226ee11adfbfb1bb748bd
vilebbit <50281195+vilebbit@users.noreply.github.com>
2025-11-23 16:02:42 +0000

    fix: CSP security rules

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       new URL("https://img.vibbit.me/**"),
       new URL("https://pics.dmm.co.jp/**"),
+      new URL("https://pics.dmm.com/**"),
       new URL("https://www.suruga-ya.jp/database/pics/game/**"),
       new URL("https://img.dlsite.jp/**"),
     ],
@@ -19,7 +20,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
               connect-src 'self' *.vibbit.me;
-              img-src 'self' blob: data: img.vibbit.me pics.dmm.co.jp www.suruga-ya.jp img.dlsite.jp;
+              img-src 'self' blob: data: img.vibbit.me pics.dmm.co.jp pics.dmm.com www.suruga-ya.jp img.dlsite.jp;
             `.replace(/\s{2,}/g, " ").trim(),
           },
         ],
